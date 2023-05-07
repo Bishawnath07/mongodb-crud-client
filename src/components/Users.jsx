51a7ad763b010462fdfafddfd773442d87b122d9
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-05-07 21:33:01 +0600

    complete client site

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
@@ -30,7 +30,11 @@ const Users = () => {
                 {
                     users.map(user => <p 
                         key={user._id}
-                        > {user.name} : {user.email} {user._id} <button onClick={()=> handleDelete(user._id)}>X</button>
+                        > {user.name} : {user.email} {user._id} 
+                         <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
+                        <button onClick={()=> handleDelete(user._id)}>X</button>
 
                           </p>)
                 }
